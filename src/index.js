1b0468e6a80f5706cf00a6efc32f2b07361a357f
LeoSanGo <souoleo@gmail.com>
2021-10-01 11:42:09 -0300

    Test spread operator

--- src/index.js
+++ src/index.js
@@ -16,3 +16,9 @@ const change = "name";
 obj.person[change] = "Alves";
 
 console.log(obj.person.name);
+//---------------------------------------
+//Spread test
+let dontChangeMe = ["Apples", "Peaches", "Detergent", "Flowers"];
+let meNeither = ["A shiny red polo", "coffee", "milk"];
+let shoppingList = [...dontChangeMe, "diapers", ...meNeither.slice(1)];
+console.table(dontChangeMe, meNeither, shoppingList);
